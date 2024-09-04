e1af65eb0e3d0960a3f38c77aa53eb643714db3a
Atsuya Watanabe <watanabe@osakafu-u.net>
2024-09-04 10:29:39 +0900

    Initial commit

---
+++ src/OneTemp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+from machine import Pin
+import onewire, ds18x20, time
+# https://github.com/raspberrypi/pico-micropython-examples/blob/master/adc/temperature.py
+# https://teratail.com/questions/buag9os7aoj9en
+import machine
+import utime
+
+def getOneTemp(PIN):
+    ow = onewire.OneWire(Pin(PIN))# 1-Wire path
+    ds = ds18x20.DS18X20(ow)# ds18x20 class instance
+    ## get rom code
+    roms = ds.scan()        # 64bit roms <class 'list'>
+    print('roms=',roms)
+    rom=roms[0]# Set [0] because there is only DS18B20 for 1-Wire
+    result = []
+    for i in range(10):
+        temp = ds.convert_temp()       # 温度データをSCRATCHPADに保存
+        time.sleep_ms(750)      # 分解能12bitの場合
+        temp=ds.read_temp(rom)  #　摂氏温度で取得
+        print(temp)
+        utime.sleep_ms(10)
+        result.append(temp)
+    return result
+def aveOneTemp(PIN):
+    list = getOneTemp(PIN)
+    print(list)
+    avg = sum(list) / len(list)
+    # print("avg: " + str(avg))
+    return avg
---
+++ src/blink.py
@@ -0,0 +1,11 @@
+import machine
+import time
+LED = machine.Pin("LED", machine.Pin.OUT)
+
+def blink(SIG,DUR):
+    for i in range(SIG):
+        LED.value(0)# Turn off now
+        time.sleep(DUR)# Wait 0.5s
+        LED.value(1)
+        time.sleep(DUR)
+        LED.value(0)
