f5bf65196e3602f2589c0f4710b822395e4c8b45
atsuyaw <68371029+atsuyaw@users.noreply.github.com>
2025-05-18 15:22:13 +0900

    feat: measure weight using hx711

--- src/RPIoTlogger/main.py
+++ src/RPIoTlogger/main.py
@@ -14,10 +14,21 @@ from rawADC import *
 # ORG_ID =
 # BUCKET =
 # ACCESS_TOKEN =
+from machine import Pin
+from hx711 import *
+import time
 from wifi import *
 
 print("IP: " + connect())
 MAC = getMac()
+
+hx = hx711(Pin(14), Pin(15))
+hx.set_power(hx711.power.pwr_up)
+hx.set_gain(hx711.gain.gain_128)
+hx.set_power(hx711.power.pwr_down)
+hx711.wait_power_down()
+hx.set_power(hx711.power.pwr_up)
+hx711.wait_settle(hx711.rate.rate_10)
 
 while True:
     INT_TEMP = aveIntTemp()
@@ -31,10 +42,12 @@ while True:
     VOL = aveRawV(ADC_VOL_PIN,COEFF_VOL,SHIFT_VOL)
     ONE_TEMP_PIN = 12
     ONE_TEMP = aveOneTemp(ONE_TEMP_PIN)
+    WEIGHT = hx.get_value()
     DATUM = f'int_temp={INT_TEMP},'\
             + f'one_temp={ONE_TEMP},'\
             + f'current={CUR},'\
             + f'voltage={VOL},'\
+            + f'weight={WEIGHT},'\
             + f'app="{APP}",'\
             + f'ver="{VER}"'
     postAPI(MAC, DATUM)
